0cd3145551e1249d4d89bdaef5868714b39811fb
e <18009838166@163.com>
2020-10-13 06:45:23 +0800

    leetcode 0001 TwoSum

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# leet-code
-leet-code algorithms learning
+# leet-code 学习
+| 序号 | 题目名称                                              | 实现代码 | 难易程度 |
+| ---- | ----------------------------------------------------- | -------- | -------- |
+| 1    | [两数之和](https://leetcode-cn.com/problems/two-sum/) |          | 容易     |
+
---
+++ src/main/java/com/github/n0001/TwoSum.java
@@ -0,0 +1,21 @@
+package com.github.n0001;
+
+/**
+ * @author MarkShen
+ * @reference https://leetcode-cn.com/problems/two-sum/
+ * @since 2020.10.13
+ */
+public class TwoSum {
+
+    static int[] twoSum(int[] arr, int target) {
+        if (arr == null || arr.length < 2)
+            return null;
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 6, 5, 9};
+        twoSum(arr, 10);
+    }
+}
